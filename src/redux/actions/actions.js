c2595909e5778f452d94d33cc7006d20442b2ea8
foram patel <p.foram147@gmail.com>
2021-11-26 13:10:36 +0000

    artist done

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,1 +1,3 @@
-export const GET_ARTIST_DATA = 'GET_ARTIST_DATA';
+export const GET_ARTIST_DATA = "GET_ARTIST_DATA";
+export const GET_ARTIST = "GET_ARTIST";
+export const GET_SONGS = "GET_SONGS";
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,23 +2,17 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import homeReducers from "../reducers/homeReducers";
-
-
-
+import artistReducers from "../reducers/artistReducers";
 const mainReducer = combineReducers({
   home: homeReducers,
+  artist: artistReducers,
 });
 
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-
 
 const configureStore = createStore(
   mainReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
-
-
 
 export default configureStore;
